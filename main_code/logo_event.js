f01540ff5025b42d71f86e9df1b50949798b3a2f
jinwoo8420 <1223wlsdn@naver.com>
2022-05-17 22:21:17 +0900

    0517

--- main_code/logo_event.js
+++ main_code/logo_event.js
@@ -1,16 +1,1 @@
-// window.onload = function () {
-//   const btn = document.getElementById("baseball");
-//   const logo = document.getElementById("logo_img");
 
-//   btn.addEventListener("mouseover", function () {
-//     // logo.translate("500px, -500px"), scale(0.6), rotate("200deg");
-//     logo.translate("500px, -500px");
-//   });
-// };
-
-// const btn = document.getElementById("baseball");
-
-// btn.addEventListener("mouseover", (event) => {
-//   // logo.translate("500px, -500px"), scale(0.6), rotate("200deg");
-//   btn.translate("500px, -500px");
-// });
